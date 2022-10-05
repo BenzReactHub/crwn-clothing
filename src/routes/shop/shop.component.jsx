bd41c78cfa54f9126c5bd46c3dd38ab36bace854
BenChang <ben.chang@chainss.com.tw>
2022-10-05 14:23:25 +0800

    Business Logic in Our Selectors

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,15 +5,15 @@ import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 import { getCategoriesAndDocments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.action'
+import { setCategories } from '../../store/categories/category.action'
 
 const Shop = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
     const getCategoriesmap = async () => {
-        const categoryMap = await getCategoriesAndDocments()
-        dispatch(setCategoriesMap(categoryMap))
+        const categoriesArray = await getCategoriesAndDocments()
+        dispatch(setCategories(categoriesArray))
     }
     getCategoriesmap()
 }, [])
