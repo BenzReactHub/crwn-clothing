f636bcfc822cbac043d8d1fbdb635e0a87f1b4d1
benzhub <lenrich1403@gmail.com>
2022-10-09 12:35:00 +0800

    test netlify functions

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import { selectCartItems } from '../../store/cart/cart.selector';
 import { selectCartTotal } from '../../store/cart/cart.selector';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 import {
   CheckoutContainer,
@@ -11,9 +12,16 @@ import {
   Total,
 } from './checkout.styles';
 
+import axios from "axios";
+
 const Checkout = () => {
   const cartItems = useSelector(selectCartItems)
   const cartTotal = useSelector(selectCartTotal)
+
+  const handleTest = async() => {
+    const test_data = await axios.get('/.netlify/functions/test')
+    console.log(test_data)
+  }
 
   return (
     <CheckoutContainer>
@@ -38,6 +46,8 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm/>
+      <div onClick={handleTest}>test click</div>
     </CheckoutContainer>
   );
 };
